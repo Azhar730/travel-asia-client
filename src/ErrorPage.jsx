f16a873165a69ca3d0dd2d574c55b51055de113b
Azhar730 <azharmahmud730@gmail.com>
2024-04-30 19:41:47 +0600

    all done

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
         <div>
             <h1 className="text-[500px] font-extrabold text-center">404</h1>
+            <div className="text-center">
+                <Link to={'/'} className="btn bg-[#D2B48C] text-center mt-10">Go Home</Link>
+            </div>
         </div>
     );
 };
