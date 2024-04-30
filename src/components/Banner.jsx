e39e2325221b4bdc7a5b41b28bb92d193d0405d6
Azhar730 <azharmahmud730@gmail.com>
2024-04-30 10:17:14 +0600

    banner section done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -16,8 +16,7 @@ const Banner = () => {
                 <img src="https://i.postimg.cc/Kc9SRCxr/bangkok-2.jpg" className="w-full rounded-lg h-[560px]" />
                 <div className="absolute rounded-lg inset-0 bg-black opacity-50"></div>
                 <div className="text-center px-20 mt-40 absolute text-white">
-                    <h3 className="text-4xl font-bold text-[#bfed07]">Find Your Dream Home with DwellDreams</h3>
-                    <p>Discover the perfect home for you. Explore diverse listings tailored to your needs and preferences. With intuitive search features and comprehensive details, finding your dream home has never been easier. Start your journey today.</p>
+                <h3 className="text-4xl font-bold text-[#f6f5f3]">Embark on Epic Journeys with AdventureAxis: Explore, Experience, Discover!</h3>
                 </div>
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle">❮</a>
@@ -25,11 +24,10 @@ const Banner = () => {
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-                <img src="https://i.postimg.cc/Gtm8bBcv/city-center-appertments.jpg" className="w-full rounded-lg h-[560px]" />
+                <img src="https://i.postimg.cc/L80n2HZb/hang-long-bay-vietnam.webp" className="w-full rounded-lg h-[560px]" />
                 <div className="absolute rounded-lg inset-0 bg-black opacity-50"></div>
                 <div className="text-center px-20 mt-40 absolute text-white">
-                    <h3 className="text-4xl font-bold text-[#bfed07]">Find Your Dream Home with DwellDreams</h3>
-                    <p>Discover the perfect home for you. Explore diverse listings tailored to your needs and preferences. With intuitive search features and comprehensive details, finding your dream home has never been easier. Start your journey today.</p>
+                <h3 className="text-4xl font-bold text-[#f6f5f3]">Embark on Epic Journeys with AdventureAxis: Explore, Experience, Discover!</h3>
                 </div>
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide2" className="btn btn-circle">❮</a>
@@ -37,11 +35,10 @@ const Banner = () => {
                 </div>
             </div>
             <div id="slide4" className="carousel-item relative w-full">
-                <img src="https://i.postimg.cc/P5sM60b0/4.jpg" className="w-full rounded-lg h-[560px]" />
+                <img src="https://i.postimg.cc/0ytJjYhZ/Angkor-Wat-cambodia.webp" className="w-full rounded-lg h-[560px]" />
                 <div className="absolute rounded-lg inset-0 bg-black opacity-50"></div>
                 <div className="text-center px-20 mt-40 absolute text-white">
-                    <h3 className="text-4xl font-bold text-[#bfed07]">Find Your Dream Home with DwellDreams</h3>
-                    <p>Discover the perfect home for you. Explore diverse listings tailored to your needs and preferences. With intuitive search features and comprehensive details, finding your dream home has never been easier. Start your journey today.</p>
+                <h3 className="text-4xl font-bold text-[#f6f5f3]">Embark on Epic Journeys with AdventureAxis: Explore, Experience, Discover!</h3>
                 </div>
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide3" className="btn btn-circle">❮</a>
